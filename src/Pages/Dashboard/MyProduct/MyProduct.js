74e6105cc97592cbf0238d048d884876e65b0d3c
Shahriyar001 <shahriyarmahbub55@gmail.com>
2022-11-28 20:30:30 +0600

    added table and data in dashboard

---
+++ src/Pages/Dashboard/MyProduct/MyProduct.js
@@ -0,0 +1,73 @@
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
+
+const MyProduct = () => {
+    const { user } = useContext(AuthContext);
+
+    const url = `http://localhost:5000/bookings?email=${user?.email}`;
+
+    const { data: bookings = [] } = useQuery({
+        queryKey: ['bookings', user?.email],
+        queryFn: async () => {
+            const res = await fetch(url,
+                //     {
+                //     headers: {
+                //         authorization: `bearer ${localStorage.getItem('accessToken')}`
+                //     }
+                // }
+            );
+            const data = await res.json();
+            return data;
+        }
+    })
+
+    return (
+        <div>
+            <h3 className='text-3xl mb-5'>My Products</h3>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Product</th>
+                            <th>price</th>
+                            <th>Location</th>
+                            {/* <th>Payment</th> */}
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            bookings?.length &&
+                            bookings?.map((booking, i) => <tr key={booking._id}>
+                                <th>{i + 1}</th>
+                                <td>{booking.buyer}</td>
+                                <td>{booking.phone}</td>
+                                <td>{booking.resale}</td>
+                                <td>{booking.location}</td>
+                                {/* <td>
+                                    {
+                                        booking.price && !booking.paid && <Link to={`/dashboard/payment/${booking._id}`}>
+                                            <button className='btn btn-primary'>Pay</button>
+                                        </Link>
+                                    }
+                                    {
+                                        booking.price && booking.paid && <span className='text-primary btn-sm'>Paid</span>
+                                    }
+                                </td> */}
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    );
+};
+
+export default MyProduct;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
+import DashboardLayout from "../../Layout/DashboardLayout";
 import Main from "../../Layout/Main";
 import Dashboard from "../../Pages/Dashboard/Dashboard/Dashboard";
+import MyProduct from "../../Pages/Dashboard/MyProduct/MyProduct";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import Product from "../../Pages/Products/Product/Product";
@@ -32,7 +34,13 @@ const router = createBrowserRouter([
     },
     {
         path: '/dashboard',
-        element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>
+        element: <PrivateRoute><DashboardLayout></DashboardLayout></PrivateRoute>,
+        children: [
+            {
+                path: '/dashboard',
+                element: <MyProduct></MyProduct>
+            }
+        ]
     }
 ])
 
